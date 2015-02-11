e9a2ee20303064d920330f3111eb82a8b32f3f3c
Wilmer Cotrina <vsigmaxz@hotmail.com>
2015-02-11 09:44:31 -0500

    Revert "Revert "IP""
    This reverts commit 806a9f3d724be7f6d1c7da3db15fed972b157256.

--- js/ng/ng.app.js
+++ js/ng/ng.app.js
@@ -565,7 +565,7 @@ function controladorPrincipal($scope,$http){
 		$scope._matriz= new Matriz();
 		$scope._habilidad= new Habilidad();
 		$scope._habilidades= [];
-		$scope.IP="10.77.33.117:8083"	
+		$scope.IP=IP;	
 
 
 	//$scope.IP="10.77.33.117:8083"
@@ -601,6 +601,8 @@ function controladorPrincipal($scope,$http){
 					}).success(function(data)
 						{
 							$scope._persona.nombres=data.nombres;
+							$scope._persona.apepat=data.apepat;
+							$scope._persona.apemat=data.apemat;
 							$scope._persona.apepat=data.apepat;
 						});
 		}
